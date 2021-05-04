f66decc386e1f5f8cb5e350d1674664212a805c3
reuvenk <Reuvenk@britannica-ks.com>
2021-05-04 14:46:14 +0300

    Add todos

--- Infrastructure/Mongo/EntityConfigurations/CityMap.cs
+++ Infrastructure/Mongo/EntityConfigurations/CityMap.cs
@@ -10,10 +10,13 @@ namespace MongoPocWebApplication1.Infrastructure.Mongo.EntityConfigurations
         {
             BsonClassMap.RegisterClassMap<City>(cm =>
             {
+                //TODO: check how to work without automap, 
                 cm.AutoMap();
+                //TODO: check how to work with SetIgnoreExtraElements false & , 
+                // as explained in https://mongodb.github.io/mongo-csharp-driver/2.12/reference/bson/mapping/
                 cm.SetIgnoreExtraElements(true);
                 cm.SetIsRootClass(true);
-                //Example of- Setting the ID and Using the GUID ID Generator
+                //Example of- Setting the ID and Using the GUID ID Generator cm.MapExtraElementsMember(c => c.CatchAll); 
                 cm.MapIdMember(c => c.Id).SetElementName("_id").SetIdGenerator(CombGuidGenerator.Instance); ;
                 cm.GetMemberMap(c => c.Name).SetElementName("name");
                 cm.GetMemberMap(c => c.PopulationCount).SetElementName("population");
